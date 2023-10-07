a559848be2da21a4b90534093e4ec44dda9e01dd
Shane Smith <110691416+ssmith34@users.noreply.github.com>
2023-10-06 20:28:04 -0400

    still need to add components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import ProductDetailPage from "./pages/ProductDetails";
+
+const router = createBrowserRouter([
+  {
+    path: '/root',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '', element: <HomePage /> },
+      { path: 'products', element: <ProductsPage /> },
+      { path: 'products/:productId', element: <ProductDetailPage /> }
+    ]
+  }
+])
+
 function App() {
   return <div></div>;
 }
